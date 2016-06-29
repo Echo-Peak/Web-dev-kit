c6fcd97e2ed908d5c074d82b1c1803cc22ffa30a
echo-peak <echopeakdev@gmail.com>
2016-06-29 14:49:06 -0400

    tools used by node processes

--- tools/installer.js
+++ tools/installer.js
@@ -1,13 +1,23 @@
-var createInstaller = require('electron-installer-squirrel-windows')
+var createInstaller = require('electron-installer-squirrel-windows');
+
+let args = process.argv.slice(2).map(e => e.replace(/"/g,''));
 var opts = {
-  path:'./some app i created',
-  out:'./OUT',
-  name:'someapp',
-  product_name:'FOOBAR',
-  authors:'red snake studios',
-  description:'this is not bad',
-  title:'floops',
-  setup_icon:'./error32px.ico',
-  version:'1.18'
+  path:args[0],
+  out:args[1],
+  name:args[2],
+  product_name:args[3],
+  authors:args[4],
+  description:args[5],
+  title:args[6],
+  setup_icon:args[7],
+  version:args[8] || "1.0"
 }
-createInstaller(opts, function done (err) { })
+
+createInstaller(opts, function done (err) {
+    console.log('creating electron installer for 'opts.name)
+  if(err){
+    console.log('ERROR: creating installer',err)
+    process.exit()
+  }
+
+ })
