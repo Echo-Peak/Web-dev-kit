5100b2896861250b9d0fff6a05179ff6504cbdd4
echo-peak <echopeakdev@gmail.com>
2016-06-29 14:55:37 -0400

    deleted

--- render/modules/namespace/local-storage-setup.js
+++
@@ -1,9 +0,0 @@
-export default function(){
-  if(localStorage.lastProject == '' || localStorage.lastProject == null || !localStorage.lastProject || localStorage.lastProject == void 0){
-    localStorage.lastProject = ''
-  }
-  if(localStorage.projectList == '' || localStorage.projectList == null || !localStorage.projectList || localStorage.projectList == void 0){
-    localStorage.projectList = "[]"
-  }
-
-}
