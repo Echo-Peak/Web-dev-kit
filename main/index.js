4c2e2d25636bcf3ca66af71b70015c547d591b42
echo-peak <echopeakdev@gmail.com>
2016-06-29 14:51:35 -0400

    cleanup & abstaction

--- main/index.js
+++ main/index.js
@@ -1,45 +1,33 @@
 'use strict';
-
-var windowManager = require('./windows');
-var net = require('net');
+// const WINDOWS = {
+//   main:require('./windows/main-window')
+// };
+const Server = require('./tools/server'); Server.connect(3000);
 const electron = require('electron');
 const app = electron.app;
-const ipcMain = electron.ipcMain;
-var child_process = require('child_process');
-var setJumpList = require('./jump-list');
-var mainPort  = 3000;
-
-var io = require('socket.io-client');
-var socket = io.connect('http://localhost:'+mainPort, {reconnect: true});
-socket.on('connect',function(sock){
-  console.log('electron connected')
-
-});
-socket.on('restart',e => {
-  socket.emit('restart-electron')
-    app.exit(0);
-});
-
-socket.on('quit' ,app.quit)
-
-
-let window = new windowManager();
-
+const setJumpList = require('./jump-list');
+const {BrowserWindow} = electron;
+const path = require('path');
 app.setName('Web developer Kit(WDK)');
 app.setUserTasks(setJumpList);
 
+
 app.on('ready', function(){
-  window.load('main')
+  //WINDOWS.main.loadURL(`./render/windows/main/index.html`);
+  var main = new BrowserWindow({
+    width:1400,
+    height:1000,
+    show:true,
+    center:true
+  });
+
+  main.loadURL(path.resolve(process.cwd() ,'render/windows/main/main.html'));
 });
 
 app.on('window-all-closed', function () {
 
-  if (process.platform !== 'darwin') {
-
-  }
-  //  app.quit();
-    socket.emit('on-quit')
-      socket.emit('quit')
+    Server.getSocket().emit('on-quit');
+    Server.getSocket().emit('quit');
 });
 
 app.on('activate', function(){
@@ -48,7 +36,7 @@ app.on('activate', function(){
 
 
 app.on('before-quit',function(){
- socket.emit('on-quit')
-  socket.emit('quit')
+ Server.getSocket().emit('on-quit');
+  Server.getSocket().emit('quit');
  process.nextTick(app.exit.bind(null ,0))
 })
