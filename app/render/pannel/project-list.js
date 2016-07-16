0922af3b539b8fe5cbda9c665c4a055284370c8d
echo-peak <echopeakdev@gmail.com>
2016-07-16 07:05:52 -0400

    setting up list view for pannel

--- app/render/pannel/project-list.js
+++ app/render/pannel/project-list.js
@@ -1,22 +1,18 @@
 import loadTemplate from '../services/loadTemplate';
 
-export default function projectList($compile){
+export default function projectList($compile , database){
   let styles = 'text-align:center; padding-top:10px;';
   let defaultTemplate = `<h4 style='${styles}'>nothing to show</h4>`;
-  let listTemplate  = loadTemplate('pannel','list_item');
+  let listTemplate  = require('raw!./view/list-item.html');
+
+  console.error('USE SCOPE BROADCAST HERE')
 
     return{
       restric:'E',
       link:function(scope ,elm ,attr){
+      //  listTemplate = $compile(listTemplate)(scope);
+      //  elm[0].appendChild(listTemplate[0])
 
-      //   if(scope.Pannel.willRenderList()){
-      //     console.log('yep' ,scope.Pannel.willRenderList() )
-      //   listTemplate = $compile(listTemplate)(scope);
-      //   elm[0].appendChild(listTemplate[0])
-      // }else{
-      //   elm[0].innerHTML = defaultTemplate
-      // }
     }
   }
 }
-//["1" ,{"name":"Falcon","files":"2","icon":"../../../res/angular.png","build":"Native","version":"0.79","lastModified":1467220345125.1033}]
