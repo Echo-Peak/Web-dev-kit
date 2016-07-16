217f058469a4a17ed3b12d712307323ab6d10644
echo-peak <echopeakdev@gmail.com>
2016-07-16 06:56:45 -0400

    added database facade

--- app/render/namespace/WDK.js
+++ app/render/namespace/WDK.js
@@ -2,27 +2,46 @@ import angularBootstrap from './angular-bootstrap';
 import localStorageSetup from './local-storage-setup';
 import Statefull_store from './statefull_store';
 import setupPrototypes from './prototype-setup';
+let DB = require('node-localdb');
 
 function setupEnv(){
-  process.env._windows = process.env.INIT_CWD+'/render/windows';
-  process.env._res = process.env.INIT_CWD+'/res';
-  process.env._compiled = process.env.INIT_CWD+'/render/compiled';
-  process.env._modules = process.env.INIT_CWD+'/render/modules';
-  process.env._main = process.env.INIT_CWD+'/render/windows/main';
-  process.env._projectCreator = process.env.INIT_CWD+'/render/windows/project-creator';
+  process.env._res = process.env.INIT_CWD+'/app/res';
+  process.env._compiled = process.env.INIT_CWD+'/app/render/compiled';
+  process.env.database = process.env.INIT_CWD+'/app/db';
 
 }
 class wdk{
   constructor(){
-    localStorageSetup();
+    //localStorageSetup();
     setupEnv();
+    this.db = function(){
+      let schemas = {}
+      return {
+        use(databaseName){
+        let schemasKeys = Object.keys(schemas);
 
+          if(~schemasKeys.indexOf(databaseName)){
+            return schemas[databaseName];
+          }else{
+            schemas[databaseName] = DB(`${process.env.database}/${databaseName}.json`);
+            return schemas[databaseName]
+          }
+        }
+      }
+    }
+
+
+    
     this.states = {
       init:new Statefull_store({cacheLimit:100}),
       get:e => this.states.init
     }
+    // this.deps = [
+    //   this.db(),
+    //   this.states.get()
+    // ];
 
-    this.angularApp = angularBootstrap(this.states.get()); //non angular dependancys
+    this.angularApp = angularBootstrap(this.states.get(), this.db()); //non angular dependancys
 
 
   }
