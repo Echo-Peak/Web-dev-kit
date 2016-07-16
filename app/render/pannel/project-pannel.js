209682aa8da3754040b86955b8ec6cbd25058c01
echo-peak <echopeakdev@gmail.com>
2016-07-16 07:06:31 -0400

    using require to load template

--- app/render/pannel/project-pannel.js
+++ app/render/pannel/project-pannel.js
@@ -1,11 +1,7 @@
-let fs = require('fs');
-import loadTemplate from '../services/loadTemplate';
-
 export default function(){
 
-  console.log('ran' ,__filename)
   return{
     restric:'E',
-    template:loadTemplate('pannel') || 'ERROR LOADING'
+    template:require('raw!./view/pannel.html')
   }
 }
